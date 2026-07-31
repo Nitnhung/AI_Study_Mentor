bdbaade28dbc103f0ed3236175d3e0fd2dcba0a2
DucAnk113 <136042258+DucAnk113@users.noreply.github.com>
2026-08-01 01:10:57 +0700

    Delete src directory

--- src/main/java/com/mentor/backend/repository/QuizResultRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mentor.backend.repository;
-
-import com.mentor.backend.entity.QuizResult;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizResultRepository extends JpaRepository<QuizResult, String> {
-}
--- src/main/java/com/mentor/backend/repository/SubjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mentor.backend.repository;
-
-import com.mentor.backend.entity.Subject;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubjectRepository extends JpaRepository<Subject, String> {
-}
